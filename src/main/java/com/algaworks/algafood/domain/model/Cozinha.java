a6176b1244617333c13ebce35a0f513eea052ce6
caiqueb05 <caique.bezerra@gmail.com>
2024-05-20 14:25:57 -0300

    6.2 Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,22 +1,19 @@
 package com.algaworks.algafood.domain.model;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -30,6 +27,10 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+	
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 
 
 
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,22 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Madero', 15, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Outback', 10, 2);
-insert into forma_pagamento (descricao) values ('cartão')
-insert into forma_pagamento (descricao) values ('pix')
-insert into forma_pagamento (descricao) values ('dinheiro')
-insert into permissao (nome, descricao) values ('zezinho', 'adm')
-insert into permissao (nome, descricao) values ('joaquina', 'financeiro')
-insert into permissao (nome, descricao) values ('bastiana', 'rh')
-insert into estado (nome) values ('são paulo')
-insert into estado (nome) values ('rio de janeiro')
-insert into cidade (nome, estado_id) values ('atibaia', 1)
-insert into cidade (nome, estado_id) values ('braganca paulista', 2)
+
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Madero', 15, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Outback', 10, 2);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Coco Bambu', 12, 2);
+
+insert into forma_pagamento (id, descricao) values (1, 'cartão')
+insert into forma_pagamento (id, descricao) values (2, 'pix')
+insert into forma_pagamento (id, descricao) values (3, 'dinheiro')
+
+insert into permissao (id, nome, descricao) values (1, 'zezinho', 'adm')
+insert into permissao (id, nome, descricao) values (2, 'joaquina', 'financeiro')
+insert into permissao (id, nome, descricao) values (3, 'bastiana', 'rh')
+
+insert into estado (id, nome) values (1, 'são paulo')
+insert into estado (id, nome) values (2, 'rio de janeiro')
+
+insert into cidade (id, nome, estado_id) values (1, 'atibaia', 1)
+insert into cidade (id, nome, estado_id) values (2, 'braganca paulista', 2)
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1,1), (1,2), (1,3), (2,3), (3,2), (3,3);
